72acd83effbeb20346ff81ba8c668c39e3c28c7d
Mustafa Cabi <113462464+mustafacabi@users.noreply.github.com>
2024-05-11 16:30:22 +0300

    Update verySimpleCalculatorProgram.cs

--- verySimpleCalculatorProgram.cs
+++ verySimpleCalculatorProgram.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,31 +14,37 @@ namespace mustafa
             Console.ReadLine();
         }
 
-        static int Calculator()
+        static double Calculator()
         {
             Console.WriteLine("Welcome to the Calculator Program...");
             Console.WriteLine("1 -> +\n2 -> -\n3 -> /\n4 -> *");
             Console.Write("Please enter the number of your chosen operator: ");
             int op = Convert.ToInt32(Console.ReadLine());
             Console.Write("Please enter the first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine()); ;
+            double num1 = Convert.ToDouble(Console.ReadLine()); ;
             Console.Write("Please enter the second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double result;
             if (op == 1)
             {
-                return num1 + num2;
-            }else if (op == 2)
-            {
-                return num1 - num2;
-            }else if (op == 3)
-            {
-                return num1 / num2;
-            }else
-            {
-                return num1 * num2;
+                result = num1 + num2;
             }
+            else if (op == 2)
+            {
+                result = num1 - num2;
+            }
+            else if (op == 3)
+            {
+                result = num1 / num2;
+            }
+            else
+            {
+                result = num1 * num2;
+            }
+            Console.Write("The result of the operation: ");
+            return result;
+            
         }
-        
+
     }
 }
