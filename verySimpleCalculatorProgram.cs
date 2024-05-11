25c61534f930df0b2bd80dfd7e7b00ad3e48dcd3
Mustafa Cabi <113462464+mustafacabi@users.noreply.github.com>
2024-05-11 17:18:46 +0300

    Update verySimpleCalculatorProgram.cs

--- verySimpleCalculatorProgram.cs
+++ verySimpleCalculatorProgram.cs
@@ -14,17 +14,21 @@ namespace mustafa
             Console.ReadLine();
         }
 
-        static double Calculator()
+        static string Calculator()
         {
             Console.WriteLine("Welcome to the Calculator Program...");
             Console.WriteLine("1 -> +\n2 -> -\n3 -> /\n4 -> *");
+
             Console.Write("Please enter the number of your chosen operator: ");
             int op = Convert.ToInt32(Console.ReadLine());
+            
             Console.Write("Please enter the first number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine()); ;
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
             Console.Write("Please enter the second number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
             double result;
+
             if (op == 1)
             {
                 result = num1 + num2;
@@ -37,12 +41,17 @@ namespace mustafa
             {
                 result = num1 / num2;
             }
-            else
+            else if (op == 4)
             {
                 result = num1 * num2;
             }
+            else
+            {
+                string x = "Invalid operator.";
+                return x;
+            }
             Console.Write("The result of the operation: ");
-            return result;
+            return Convert.ToString(result);
             
         }
 
