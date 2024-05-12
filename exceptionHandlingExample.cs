228cd68718a4a4e79889a0047d05aac904306c42
Mustafa Cabi <113462464+mustafacabi@users.noreply.github.com>
2024-05-12 13:22:28 +0300

    Create exceptionHandlingExample.cs

---
+++ exceptionHandlingExample.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch 
+            {
+                Console.WriteLine("Error...");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
